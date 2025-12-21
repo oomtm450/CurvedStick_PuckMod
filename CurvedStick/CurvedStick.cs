5654a7d7250f0dbbd141d91baf980a6e4fbeb920
MathieuC <oomtm453@gmail.com>
2025-12-20 20:23:09 -0500

    Refactoring.

--- CurvedStick/CurvedStick.cs
+++ CurvedStick/CurvedStick.cs
@@ -510,12 +510,13 @@ namespace oomtm450PuckMod_CurvedStick {
 
                         foreach (Player _player in PlayerManager.Instance.GetPlayers(true).Where(x => x.IsReplay.Value)) {
                             Player notReplayPlayer = PlayerManager.Instance.GetPlayerByClientId(_player.OwnerClientId - REPLAY_PLAYER_OFFSET);
-                            if (notReplayPlayer) {
-                                if (notReplayPlayer.IsLocalPlayer)
-                                    SetCurvedStick(_player, ClientConfig);
-                                else
-                                    SetCurvedStick(_player, _playersCurve[_player.OwnerClientId - REPLAY_PLAYER_OFFSET]);
-                            }
+                            if (!notReplayPlayer)
+                                continue;
+
+                            if (notReplayPlayer.IsLocalPlayer)
+                                SetCurvedStick(_player, ClientConfig);
+                            else
+                                SetCurvedStick(_player, _playersCurve[_player.OwnerClientId - REPLAY_PLAYER_OFFSET]);
                         }
                         break;
 
