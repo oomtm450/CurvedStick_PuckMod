3f2bc448b442230eeb3a6dbe2eff1dde6ec9ac93
MathieuC <oomtm453@gmail.com>
2026-01-04 17:39:08 -0500

    Changed mod version to 0.3.1.

--- CurvedStick/CurvedStick.cs
+++ CurvedStick/CurvedStick.cs
@@ -19,7 +19,7 @@ namespace oomtm450PuckMod_CurvedStick {
         /// <summary>
         /// Const string, version of the mod.
         /// </summary>
-        private const string MOD_VERSION = "0.3.0";
+        private const string MOD_VERSION = "0.3.1";
 
         /// <summary>
         /// List of string, last released versions of the mod.
@@ -28,6 +28,7 @@ namespace oomtm450PuckMod_CurvedStick {
             "0.1.0",
             "0.2.0",
             "0.2.1",
+            "0.3.0",
         });
 
         private const string ASK_SERVER_FOR_DATA = Constants.MOD_NAME + "ASKDATA";
