37e68e5ccaac0537e3d033d4bbb479ebe8fed272
MathieuC <oomtm453@gmail.com>
2026-04-10 15:03:58 -0400

    Adjusted for new Puck update (312).

--- CurvedStick/Constants.cs
+++ CurvedStick/Constants.cs
@@ -3,7 +3,7 @@ namespace oomtm450PuckMod_CurvedStick {
         /// <summary>
         /// Const string, added current Puck Application.version to check for mod compatibility.
         /// </summary>
-        internal const string CURRENT_APPLICATION_VERSION = "310";
+        internal const string CURRENT_APPLICATION_VERSION = "312";
 
         internal const string WORKSHOP_MOD_NAME = "Curved Stick";
 
