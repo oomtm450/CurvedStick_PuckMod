cd85e6c194295c51aa63625c6e932c84d8ac1edf
MathieuC <oomtm453@gmail.com>
2026-07-02 14:38:06 -0400

    Adjusted for B1117.

--- CurvedStick/CurvedStick.cs
+++ CurvedStick/CurvedStick.cs
@@ -20,7 +20,7 @@ namespace oomtm450PuckMod_CurvedStick {
         /// <summary>
         /// Const string, version of the mod.
         /// </summary>
-        private const string MOD_VERSION = "0.3.4a";
+        private const string MOD_VERSION = "0.3.5";
 
         /// <summary>
         /// List of string, last released versions of the mod.
@@ -34,6 +34,7 @@ namespace oomtm450PuckMod_CurvedStick {
             "0.3.2",
             "0.3.3",
             "0.3.4",
+            "0.3.4a",
         });
 
         private const string ASK_SERVER_FOR_DATA = Constants.MOD_NAME + "ASKDATA";
